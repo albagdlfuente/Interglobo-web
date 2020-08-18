d4c4c462d85c7c71178a2f09f33af27986ebb548
albagdlfuente <69843888+albagdlfuente@users.noreply.github.com>
2020-08-18 11:01:57 +0200

    commit inicial
    Se ha subido todo el codigo de la web al repositorio

---
+++ public_html/interglobo.interglobo.net/cursos/wp-content/plugins/woocommerce-eu-vat-compliance/eu-vat-compliance.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Plugin Name: WooCommerce EU VAT Compliance (Free)
+Plugin URI: https://www.simbahosting.co.uk/s3/product/woocommerce-eu-vat-compliance/
+Description: Provides features to assist WooCommerce with EU VAT compliance
+Version: 1.11.3
+Text Domain: woocommerce-eu-vat-compliance
+Domain Path: /languages
+Author: David Anderson
+Author URI: https://www.simbahosting.co.uk/s3/shop/
+Requires at least: 3.3
+Tested up to: 4.7
+License: GNU General Public License v3.0
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+Copyright: 2014- David Anderson
+Portions licenced under the GPL v3 from other authors
+*/
+
+// The present file has minimal content to avoid needing to duplicate code changes in the free/premium versions (whose headers, above, can differ), whilst keeping both in the same source control
+
+if (!defined('ABSPATH')) die('Access denied.');
+require(dirname(__FILE__).'/bootstrap.php');
